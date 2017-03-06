96d059072c53bb650bbc292726930e762305f58f
Federico Ruggieri <f.ruggieri89@gmail.com>
2017-03-06 14:10:44 +0100

    Add files via upload

---
+++ DiceServer/DiceServer/DataExchange.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+class DataExchange
+{
+    IPAddress IP;
+    Int32 P;
+
+    public DataExchange(IPAddress Addr, Int32 Port)
+    {
+        IP = Addr;
+        P = Port;
+    }
+
+    public DataExchange(string Addr, Int32 Port)
+    {
+        IP = IPAddress.Parse(Addr);
+        P = Port;
+    }
+
+    /// <summary>
+    /// Invia tramite TCPClient i dati all'ip specificato nel costruttore
+    /// </summary>
+    /// <param name="Data">
+    /// I dati da inviare in formato String
+    /// </param>
+    /// <returns>
+    /// Restituisce True se non sono avvenuti errori altrimenti False
+    /// </returns>
+    public Boolean Send(string Data)
+    {
+        TcpClient TC = new TcpClient();
+
+        try
+        {
+            TC.Connect(IP, P);
+        }
+        catch
+        {
+            return false;
+        }
+
+        try
+        {
+            NetworkStream NS = TC.GetStream();
+
+            if (NS.CanRead && NS.CanWrite)
+            {
+                Byte[] B;
+                B = Encoding.ASCII.GetBytes(Data);
+
+                NS.Write(B, 0, B.Length);
+            }
+            TC.Close();
+        }
+        catch
+        {
+            TC.Close();
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Invia tramite TCPClient i dati all'ip specificato nel costruttore
+    /// </summary>
+    /// <param name="Data">
+    /// I dati da inviare in formato Byte[]
+    /// </param>
+    /// <returns>
+    /// Restituisce True se non sono avvenuti errori altrimenti False
+    /// </returns>
+    public Boolean Send(byte[] Data)
+    {
+        TcpClient TC = new TcpClient();
+
+        try
+        {
+            TC.Connect(IP, P);
+        }
+        catch
+        {
+            return false;
+        }
+
+        try
+        {
+            NetworkStream NS = TC.GetStream();
+
+            if (NS.CanRead && NS.CanWrite)
+            {
+                NS.Write(Data, 0, Data.Length);
+            }
+            TC.Close();
+        }
+        catch
+        {
+            TC.Close();
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Avvia le funzioni di server e resta in attesa
+    /// dei dati da ricevere sull'ip e la porta specificati
+    /// nel costruttore della classe
+    /// Solitamente si inserisce all'interno di iterazioni continue
+    /// </summary>
+    /// <param name="buffersize">
+    /// Definisce la dimensione del buffer da ricevere
+    /// </param>
+    /// <returns>
+    /// Restituisce una stringa contenente i dati ricevuti convertiti in caratteri ASCII
+    /// </returns>
+    public string ReceiveString(int buffersize = 1024)
+    {
+        string ReceiveData = "";
+        try
+        {
+            TcpListener List = new TcpListener(IP, P);
+
+            List.Start();
+
+            Socket s = List.AcceptSocket();
+
+            byte[] b = new byte[buffersize];
+            int k = s.Receive(b);
+
+            ReceiveData = Encoding.ASCII.GetString(b);
+
+            //Rimozione dei dati superflui
+            int i = ReceiveData.IndexOf((char)0);
+            if (i > 0) ReceiveData = ReceiveData.Substring(0, i);
+
+            s.Close();
+            List.Stop();
+        }
+        catch
+        {
+            return "";
+        }
+
+        return ReceiveData;
+    }
+
+    /// <summary>
+    /// Avvia le funzioni di server e resta in attesa
+    /// dei dati da ricevere sull'ip e la porta specificati
+    /// nel costruttore della classe
+    /// Solitamente si inserisce all'interno di iterazioni continue
+    /// </summary>
+    /// <param name="buffersize">
+    /// Definisce la dimensione del buffer da ricevere
+    /// </param>
+    /// <returns>
+    /// Restituisce l'array di byte ricevuto
+    /// </returns>
+    public byte[] ReceiveByteArray(int buffersize = 1024)
+    {
+        byte[] b = new byte[buffersize];
+        try
+        {
+            TcpListener List = new TcpListener(IP, P);
+
+            List.Start();
+
+            Socket s = List.AcceptSocket();
+
+            int k = s.Receive(b);
+
+            s.Close();
+            List.Stop();
+        }
+        catch
+        {
+            return new byte[0];
+        }
+
+        return b;
+    }
+
+}
---
+++ DiceServer/DiceServer/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace DiceServer
+{
+    class Program
+    {
+
+        static IPAddress MyIP = GetLocalIPAddress();
+        static int Port = 44455;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - Welcome to DiceServer!", Get_Time());
+            Console.WriteLine("{0} - ", Get_Time());
+
+            Console.WriteLine("{0} - Starting listening on {1}:{2}", Get_Time(), MyIP.ToString(), Port.ToString());
+            DataExchange D = new DataExchange(MyIP, Port);
+            Console.WriteLine("{0} - Listening...", Get_Time());
+            while (true)
+            {
+                string[] s = D.ReceiveString().Split('|');
+                
+                switch (Convert.ToInt32(s[1]))
+                {
+                    //flip a coin
+                    case 0:
+                        s[1] = "flipped a coin";
+                        break;
+                    //Roll 1d3
+                    case 1:
+                        s[1] = "rolled 1d3";
+                        break;
+                    //Roll 1d4
+                    case 2:
+                        s[1] = "rolled 1d4";
+                        break;
+                    //Roll 1d6
+                    case 3:
+                        s[1] = "rolled 1d6";
+                        break;
+                    //Roll 1d8
+                    case 4:
+                        s[1] = "rolled 1d8";
+                        break;
+                    //Roll 1d10
+                    case 5:
+                        s[1] = "rolled 1d10";
+                        break;
+                    //Roll 1d12
+                    case 6:
+                        s[1] = "rolled 1d12";
+                        break;
+                    //Roll 1d20
+                    case 7:
+                        s[1] = "rolled 1d20";
+                        break;
+                    //Roll 1d30
+                    case 8:
+                        s[1] = "rolled 1d30";
+                        break;
+                    //Roll 1d100
+                    case 9:
+                        s[1] = "rolled 1d100";
+                        break;
+                    //Roll all characteristics
+                    case 10:
+                        s[1] = "rolled new characteristics";
+                        break;
+                    //Roll 2d4
+                    case 11:
+                        s[1] = "rolled 2d4";
+                        break;
+                    //Roll 3d4
+                    case 12:
+                        s[1] = "rolled 3d4";
+                        break;
+                    //Roll 2d6
+                    case 13:
+                        s[1] = "rolled 2d6";
+                        break;
+                    //Roll 3d6
+                    case 14:
+                        s[1] = "rolled 3d6";
+                        break;
+                    //Roll 4d6
+                    case 15:
+                        s[1] = "rolled 4d6";
+                        break;
+                    //Roll 5d6
+                    case 16:
+                        s[1] = "rolled 5d6";
+                        break;
+                    //Roll 6d6
+                    case 17:
+                        s[1] = "rolled 6d6";
+                        break;
+                }
+
+                Console.WriteLine("{0} - ***************************", Get_Time());
+                Console.WriteLine("{0} - {1} {2}", Get_Time(), s[0], s[1]);
+                Console.WriteLine("{0} - with result: {1}", Get_Time(), s[2]);
+            }
+
+        }
+
+        public static IPAddress GetLocalIPAddress()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        private static string Get_Time()
+        {
+            string time = "";
+
+            int a = DateTime.Now.Hour;
+            if (a < 10) time += "0" + a;
+            else time += a;
+            time += ":";
+            a = DateTime.Now.Minute;
+            if (a < 10) time += "0" + a;
+            else time += a;
+            time += ":";
+            a = DateTime.Now.Second;
+            if (a < 10) time += "0" + a;
+            else time += a;
+
+            return time;
+        }
+    }
+}
